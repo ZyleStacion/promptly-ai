0732c88d825d8ea45c02a0e99d9ecb683328936b
giakhang2601 <giakhang25069@gmail.com>
2025-11-24 21:22:54 +0700

    formatting code

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,85 +1,125 @@
-import React from 'react'
-import HeroImg from '../../assets/Hero.png'
-import { LuBrain} from "react-icons/lu";
-import { motion } from 'framer-motion';
-import CountUp from 'react-countup';
+import React from "react";
+import HeroImg from "../../assets/Hero.png";
+import { LuBrain } from "react-icons/lu";
+import { motion } from "framer-motion";
+import CountUp from "react-countup";
 
 const Hero = () => {
   return (
+    <div className=" bg-gray-900 containers grid grid-cols-1 md:grid-cols-2 min-h-[650px] md:px-5 relative">
+      {/* brand infor */}
+      <div className=" flex flex-col mt-5 xl:pt-24 md:pt-24 px-8 pb-10 md:pr-10 xl:pr-30 ">
+        <div className=" text-center md:text-left space-y-6">
+          <motion.p
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.2 }}
+            className=" text-blue-400 font-bold flex items-center "
+          >
+            <LuBrain />
+            AI Model Training Platform
+          </motion.p>
 
-    <div className=' bg-gray-900 containers grid grid-cols-1 md:grid-cols-2 min-h-[650px] md:px-5 relative'>
-        {/* brand infor */}
-        <div className=' flex flex-col mt-5 xl:pt-24 md:pt-24 px-8 pb-10 md:pr-10 xl:pr-30 '>
-            <div className=' text-center md:text-left space-y-6'>
-                <motion.p
-                initial={{opacity: 0, x: -100}}
-                animate={{opacity: 1, x:0}}
-                transition={{duration: 0.2}}
-                 className=' text-blue-400 font-bold flex items-center '><LuBrain/>AI Model Training Platform</motion.p>
+          <motion.h1
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.4 }}
+            className=" text-white text-5xl font-semibold"
+          >
+            Train Your Own{" "}
+            <p className=" font-bold inline-block bg-gradient-to-r from-blue-600 to-violet-600 text-transparent bg-clip-text">
+              AI Model
+            </p>{" "}
+            for Your Business
+          </motion.h1>
 
-                <motion.h1
-                initial={{opacity: 0, x: -100}}
-                animate={{opacity: 1, x:0}}
-                transition={{duration: 0.4}}
-                 className=' text-white text-5xl font-semibold'>Train Your Own <p className=' font-bold inline-block bg-gradient-to-r from-blue-600 to-violet-600 text-transparent bg-clip-text'>AI Model</p> for Your Business</motion.h1>
+          <motion.p
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            className=" text-white pr-5"
+          >
+            Chat with our AI, upload your business data, and get a customized AI
+            model trained specifically for your needs. Download and deploy it
+            locally.
+          </motion.p>
 
-                <motion.p
-                initial={{opacity: 0, x: -100}}
-                animate={{opacity: 1, x:0}}
-                transition={{duration: 0.6}}
-                 className=' text-white pr-5'>Chat with our AI, upload your business data, and get a customized AI model trained specifically for your needs. Download and deploy it locally.</motion.p>
-                
-                {/* button section */}
-                <motion.div
-                initial={{opacity: 0, x: -100}}
-                animate={{opacity: 1, x:0}}
-                transition={{duration: 0.8}}
-                 className=' space-x-5'>
-                    <button className='primary-btn'>Get Started</button>
-                    <button className=' text-white underline'>Learn More</button>
-                </motion.div>
+          {/* button section */}
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8 }}
+            className=" space-x-5"
+          >
+            <button className="primary-btn">Get Started</button>
+            <button className=" text-white underline">Learn More</button>
+          </motion.div>
 
-                {/* yapping */}
-                <motion.div
-                initial={{opacity: 0, }}
-                animate={{opacity: 1, }}
-                transition={{duration: 0.5}}
-                 className='   flex space-x-10 text-white justify-center md:justify-start xl:jus' >
-                    <div>
-                        <p className=' font-bold text-3xl'> <CountUp start={0} end={500} duration={2}  enableScrollSpy={true} scrollSpyOnce={true}  /> <span className='  inline-flex'>+</span></p>
-                        <p >Businesses</p>
-                    </div>
-                    <div>
-                        <p className=' font-bold text-3xl'><CountUp start={0} end={10000} duration={2}  enableScrollSpy={true} scrollSpyOnce={true}  /> <span className='  inline-flex'>+</span></p>
-                        <p>Models Trained</p>
-                    </div>
-                    <div>
-                        <p className=' font-bold text-3xl'><CountUp start={0} end={99} duration={2} enableScrollSpy={true} scrollSpyOnce={true}  /> <span className='  inline-flex'>%</span></p>
-                        <p>Satisfaction</p>
-                    </div>
-                </motion.div>  
-
+          {/* yapping */}
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="   flex space-x-10 text-white justify-center md:justify-start xl:jus"
+          >
+            <div>
+              <p className=" font-bold text-3xl">
+                {" "}
+                <CountUp
+                  start={0}
+                  end={500}
+                  duration={2}
+                  enableScrollSpy={true}
+                  scrollSpyOnce={true}
+                />{" "}
+                <span className="  inline-flex">+</span>
+              </p>
+              <p>Businesses</p>
             </div>
+            <div>
+              <p className=" font-bold text-3xl">
+                <CountUp
+                  start={0}
+                  end={10000}
+                  duration={2}
+                  enableScrollSpy={true}
+                  scrollSpyOnce={true}
+                />{" "}
+                <span className="  inline-flex">+</span>
+              </p>
+              <p>Models Trained</p>
+            </div>
+            <div>
+              <p className=" font-bold text-3xl">
+                <CountUp
+                  start={0}
+                  end={99}
+                  duration={2}
+                  enableScrollSpy={true}
+                  scrollSpyOnce={true}
+                />{" "}
+                <span className="  inline-flex">%</span>
+              </p>
+              <p>Satisfaction</p>
+            </div>
+          </motion.div>
         </div>
+      </div>
 
+      {/* Hero image */}
 
-        {/* Hero image */}
-
-        <div className='flex justify-center items-center  '>
-            <motion.img  
-            initial={{ opacity: 0, x:200}}
-            animate={{ opacity: 1, x:0}}
-            transition={{ type: "spring", stiffness: 100, delay:0.2}}
-                src={HeroImg} 
-                alt="Ai Related Image"
-                className=' w-[550px] md:w-[550px] xl:w-[700px]'
-
-            />
-        </div>
-
+      <div className="flex justify-center items-center  ">
+        <motion.img
+          initial={{ opacity: 0, x: 200 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ type: "spring", stiffness: 100, delay: 0.2 }}
+          src={HeroImg}
+          alt="Ai Related Image"
+          className=" w-[550px] md:w-[550px] xl:w-[700px]"
+        />
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Hero
+export default Hero;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,66 +1,67 @@
-import React from 'react'
-import { NavbarMenu } from '../../mockData/data';
-import { LuBrain} from "react-icons/lu";
+import React from "react";
+import { NavbarMenu } from "../../mockData/data";
+import { LuBrain } from "react-icons/lu";
 import { MdMenu } from "react-icons/md";
-import {motion} from 'framer-motion';
-import ResponsiveMenu from './ResponsiveMenu';
+import { motion } from "framer-motion";
+import ResponsiveMenu from "./ResponsiveMenu";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = React.useState(false);
   return (
     <>
-    <nav>
+      <nav>
         <motion.div
-        initial={{opacity: 0}}
-        animate={{opacity: 1}}
-        transition={{duration: 0.5}}
-         className="containers flex justify-between items-center py-3 px-10 bg-neutral-900">
-
-            {/* logo section */}
-            <a href="/">
-              <div className=" text-2xl flex items-center gap-2 font-bold ">
-
-                  <LuBrain className="text-3xl text-white bg-gradient-to-r from-blue-600 to-violet-600 text-transparent rounded-lg p-0.5"/>
-                  <p className='text-white '>Promptly AI</p>
-      
-              </div>
-            </a>  
-
-            {/* menu section  */}
-            <div className='hidden lg:block'>
-                <ul className="flex items-center gap-6">
-                    {NavbarMenu.map((item) => {
-                      return (
-                        <li key={item.id} >
-                            <a href={item.link} className="inline-block text-white text-sm xl:text-base py-1 px-2 xl:px-3
-                             hover:text-secondary transition-all duration-300 font-semibold"> 
-                             {item.title}
-                            </a>
-                        </li>
-                      );
-                    })}    
-                </ul>
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          className="containers flex justify-between items-center py-3 px-10 bg-neutral-900"
+        >
+          {/* logo section */}
+          <a href="/">
+            <div className=" text-2xl flex items-center gap-2 font-bold ">
+              <LuBrain className="text-3xl text-white bg-gradient-to-r from-blue-600 to-violet-600 text-transparent rounded-lg p-0.5" />
+              <p className="text-white ">Promptly AI</p>
             </div>
+          </a>
 
+          {/* menu section  */}
+          <div className="hidden lg:block">
+            <ul className="flex items-center gap-6">
+              {NavbarMenu.map((item) => {
+                return (
+                  <li key={item.id}>
+                    <a
+                      href={item.link}
+                      className="inline-block text-white text-sm xl:text-base py-1 px-2 xl:px-3
+                             hover:text-secondary transition-all duration-300 font-semibold"
+                    >
+                      {item.title}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
 
-            {/* login/logout buttons section  */}
-            <div className="hidden lg:block space-x-6">
-              <button className='font-semibold text-white'>Sign In</button>
-              <button className='text-white bg-secondary font-semibold rounded-full px-6 py-2 hover:scale-110 duration-300'>Sign Up</button>
+          {/* login/logout buttons section  */}
+          <div className="hidden lg:block space-x-6">
+            <button className="font-semibold text-white">Sign In</button>
+            <button className="text-white bg-secondary font-semibold rounded-full px-6 py-2 hover:scale-110 duration-300">
+              Sign Up
+            </button>
+          </div>
 
-            </div>
-
-            {/* mobile list section  */}
-            <div className='lg:hidden' onClick={() => setIsOpen(!isOpen)} >
-              <MdMenu className='text-4xl text-white hover:font-bold'/>
-            </div>
+          {/* mobile list section  */}
+          <div className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
+            <MdMenu className="text-4xl text-white hover:font-bold" />
+          </div>
         </motion.div>
-    </nav>
+      </nav>
 
-    {/* Mobile dropdown menu  */}
-    <ResponsiveMenu isOpen={isOpen} />
+      {/* Mobile dropdown menu  */}
+      <ResponsiveMenu isOpen={isOpen} />
     </>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
